093ae01cbac6c9b2cf70cd8ec9e6a28f23c02e7e
zhouzhongliang <zhouzhongliang@babeltime.com>
2018-12-09 12:09:33 +0800

    init go

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/7891liang/go-lua-syntax-checker
--- src/lua-syntax-checker.go
+++ src/lua-syntax-checker.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"go-lua-syntax-checker/github.com/golua/lua"
+	"github.com/yuin/gopher-lua"
 	"os"
-)
-
-var (
-	trace bool = false
-	debug bool = false
-	tests bool = false
 )
 
 func main() {
 	path :=os.Args[1]
 
-	var opts = []lua.Option{lua.WithTrace(trace), lua.WithVerbose(debug)}
-	state := lua.NewState(opts...)
-	defer state.Close()
-
-
-
-	error :=state.LoadFile(path)
-	if error != nil {
-		fmt.Errorf("Lua Error %s",error)
+	L := lua.NewState()
+	defer L.Close()
+	if err := L.DoFile(path); err != nil {
+		panic(err)
 	}
 }
