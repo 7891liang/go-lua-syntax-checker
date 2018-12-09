6f35c0bb89f4e8b5a7084d0be151a4effb165c60
zhouzhongliang <zhouzhongliang@babeltime.com>
2018-12-09 11:58:29 +0800

    fix

--- src/lua-syntax-checker.go
+++ src/lua-syntax-checker.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"go-lua-syntax-checker/github.com/golua/lua"
+	"os"
+)
+
+var (
+	trace bool = false
+	debug bool = false
+	tests bool = false
+)
+
 func main() {
-	
+	path :=os.Args[1]
+
+	var opts = []lua.Option{lua.WithTrace(trace), lua.WithVerbose(debug)}
+	state := lua.NewState(opts...)
+	defer state.Close()
+
+
+
+	error :=state.LoadFile(path)
+	if error != nil {
+		fmt.Errorf("Lua Error %s",error)
+	}
 }
