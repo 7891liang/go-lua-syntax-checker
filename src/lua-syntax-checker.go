0a911468bda95db453c88aa36df3605b7d0dc788
7891liang <858834494@qq.com>
2018-12-09 15:53:13 +0800

    Update lua-syntax-checker.go

--- src/lua-syntax-checker.go
+++ src/lua-syntax-checker.go
@@ -48,11 +48,11 @@ func main() {
 
 
 func GetCurrentDirectory() string {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0])) //返回绝对路径  filepath.Dir(os.Args[0])去除最后一个元素的路径
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
 		}
-	return strings.Replace(dir, "\\", "/", -1) //将\替换成/
+	return strings.Replace(dir, "\\", "/", -1)
 }
 
 
